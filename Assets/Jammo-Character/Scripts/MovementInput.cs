87275151e1de17143f284774bb50960f63df153f
PPYukito <PanutatR@gmail.com>
2024-05-17 15:03:18 +0900

    [Add] created new scene for splatoon shooting system; [Mod] updated new movement for shooting and new gun models;

--- Assets/Jammo-Character/Scripts/MovementInput.cs
+++ Assets/Jammo-Character/Scripts/MovementInput.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using StarterAssets;
 
 //This script requires you to have setup your animator with 3 parameters, "InputMagnitude", "InputX", "InputZ"
 //With a blend tree to control the inputmagnitude and allow blending between animations.
@@ -33,18 +35,66 @@ public class MovementInput : MonoBehaviour {
     [Range(0, 1f)]
     public float StopAnimTime = 0.15f;
 
-    public float verticalVel;
+	[Header("Cinemachine")]
+	[Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
+	public GameObject CinemachineCameraTarget;
+
+	[Tooltip("For locking the camera position on all axis")]
+	public bool LockCameraPosition = false;
+
+	[Tooltip("How far in degrees can you move the camera up")]
+	public float TopClamp = 70.0f;
+
+	[Tooltip("How far in degrees can you move the camera down")]
+	public float BottomClamp = -30.0f;
+
+	[Tooltip("Camera's Speed when Rotating or Tilting")]
+	public float CameraRotateSpeed = 1.0f;
+
+	[Tooltip("Additional degress to override the camera. Useful for fine tuning camera position when locked")]
+	public float CameraAngleOverride = 0.0f;
+
+	private StarterAssetsInputs _input;
+#if ENABLE_INPUT_SYSTEM
+	private PlayerInput _playerInput;
+#endif
+
+	public float verticalVel;
     private Vector3 moveVector;
 
-	// Use this for initialization
-	void Start () {
+	// cinemachine
+	private float _cinemachineTargetYaw;
+	private float _cinemachineTargetPitch;
+
+	private const float _threshold = 0.01f;
+
+	private bool IsCurrentDeviceMouse
+	{
+		get
+		{
+#if ENABLE_INPUT_SYSTEM
+			return _playerInput.currentControlScheme == "KeyboardMouse";
+#else
+				return false;
+#endif
+		}
+	}
+
+	private void Start ()
+	{
+#if ENABLE_INPUT_SYSTEM
+		_playerInput = GetComponent<PlayerInput>();
+#else
+		Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
+#endif
+		_input = GetComponent<StarterAssetsInputs>();
+
 		anim = this.GetComponent<Animator> ();
 		cam = Camera.main;
 		controller = this.GetComponent<CharacterController> ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	private void Update () {
 		InputMagnitude ();
 
         isGrounded = controller.isGrounded;
@@ -60,7 +110,12 @@ public class MovementInput : MonoBehaviour {
         controller.Move(moveVector);
     }
 
-    void PlayerMoveAndRotation() {
+	private void LateUpdate()
+	{
+		CameraRotation();
+	}
+
+	void PlayerMoveAndRotation() {
 		InputX = Input.GetAxis ("Horizontal");
 		InputZ = Input.GetAxis ("Vertical");
 
@@ -120,6 +175,34 @@ public class MovementInput : MonoBehaviour {
 		} else if (Speed < allowPlayerRotation) {
 			anim.SetFloat ("Blend", Speed, StopAnimTime, Time.deltaTime);
 		}
+	}
+
+	private void CameraRotation()
+    {
+		// if there is an input and camera position is not fixed
+		if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
+		{
+			//Don't multiply mouse input by Time.deltaTime;
+			float deltaTimeMultiplier = IsCurrentDeviceMouse ? CameraRotateSpeed : Time.deltaTime;
+
+			_cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
+			_cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
+		}
+
+		// clamp our rotations so our values are limited 360 degrees
+		_cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+		_cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+
+		// Cinemachine will follow this target
+		CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride,
+			_cinemachineTargetYaw, 0.0f);
+	}
+
+	private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+	{
+		if (lfAngle < -360f) lfAngle += 360f;
+		if (lfAngle > 360f) lfAngle -= 360f;
+		return Mathf.Clamp(lfAngle, lfMin, lfMax);
 	}
 
 	public void ResetMovement()
